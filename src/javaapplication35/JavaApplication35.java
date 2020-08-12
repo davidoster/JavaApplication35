7a55392dd15b651d7800d66695b3ebb4eed18cd6
David Oster <paspa@hotmail.com>
2020-08-12 15:54:22 +0300

    - Still not working properly - We need to check several different strings, abc22def, abcdef22, 22abc22def22, abc22def, abc2222def

--- src/javaapplication35/JavaApplication35.java
+++ src/javaapplication35/JavaApplication35.java
@@ -27,25 +27,31 @@ public class JavaApplication35 {
         
         // 1. find if any of the numbers are substrings on the given strings
         // 2. if there are then count the occurences along with their position within the string
+        String myStr = "Hello planet earth, you are a great planet.";
+        System.out.println(myStr.indexOf("planet",7));
         
         Scanner scan = new Scanner(System.in);
+        List<Integer> occur = new ArrayList<>();
         
         int num = askNumber(scan);
-        int num2 = askNumber(scan);
+//        int num2 = askNumber(scan);
         String s1 = askString(scan);
-        String s2 = askString(scan);
+//        String s2 = askString(scan);
         
-        checkIfNumExistsInString(num, s1);
-        checkIfNumExistsInString(num2, s2);
-        
-        
+        if(checkIfNumExistsInString(num, s1)) {
+            occur = findStringOccurences(num, s1);
+        }
+        System.out.println("Occurences");
+        for (Integer integer : occur) {
+            System.out.println("Occurence in position = " + integer);
+        }
     }
     
     public static int askNumber(Scanner scan){
         
         int num;
         
-        System.out.println("Give a number");
+//        System.out.println("Give a number");
         System.out.println("/*********************************/");
         System.out.println("Give the number : ");
         num = scan.nextInt();
@@ -58,10 +64,10 @@ public class JavaApplication35 {
         
         String s;
         
-        System.out.println("Give a String");
+//        System.out.println("Give a String");
         System.out.println("/*********************************/");
         System.out.println("Give the string : ");
-        s = scan.nextLine();
+        s = scan.next();
         
         return s;
     }
@@ -71,13 +77,36 @@ public class JavaApplication35 {
         return s.contains(String.valueOf(num));
     }
     
-    public List<Integer> findStringOccurences(int num, String s) {
+    // 22
+//    strArr[0] = "22"
+//    strArr[1] = "abc"
+    //22abc22def22
+    //22 abc 22 def
+    public static List<Integer> findStringOccurences(int num, String s) {
        List<Integer> occurences = new ArrayList<>();
        // for, while (loop) searching for the num in consecutive places
+       String strNum = String.valueOf(num);
        
-       // occurences.add(x);
+       int counter = 0;
+       int c = 0;
+       while(c >= 0) {
+           if(counter == 0) {
+                counter = s.indexOf(strNum);
+                occurences.add(counter);
+                counter += strNum.length();
+           } else {
+               if(c > s.length()) c = -1;
+               c = s.indexOf(strNum, counter);
+               System.out.println(c);
+               
+               if(c > 0 && c <= s.length()) {
+                   occurences.add(c);
+                   counter += strNum.length();
+               } 
+               
+           }
+       }
        return occurences;
-        
     }
     
     
